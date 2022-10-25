749a1b3250bafbf9aca039e6146231c870f48502
battle15 <mbattle15@student.ccc.edu>
2022-10-25 14:06:58 +0000

    General functions completed Need to format the application properly, nav bar, tables, etc...

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,20 @@
 class BookmarksController < ApplicationController
+
+  before_action(:load_current_user)
+  
+  def load_current_user
+    the_id = session[ :user_id]
+    @current_user = User.where({ :id => session[:user_id ]}).at(0)
+  end
+  
   def index
-    matching_bookmarks = Bookmark.all
+    #matching_bookmarks = Bookmark.where({ :user_id => session.fetch(:user_id) })
+
+    #@current_user =User.where({ :id => session[:user_id ]}).at(0)
+
+    #self.load_current_user
+
+    matching_bookmarks = @current_user.bookmarks
 
     @list_of_bookmarks = matching_bookmarks.order({ :created_at => :desc })
 
@@ -8,6 +22,10 @@ class BookmarksController < ApplicationController
   end
 
   def show
+    #@current_user =User.where({ :id => session[:user_id ]}).at(0)
+
+    #self.load_current_user
+
     the_id = params.fetch("path_id")
 
     matching_bookmarks = Bookmark.where({ :id => the_id })
@@ -18,8 +36,12 @@ class BookmarksController < ApplicationController
   end
 
   def create
+    #@current_user =User.where({ :id => session[:user_id ]}).at(0)
+
+    #self.load_current_user
+
     the_bookmark = Bookmark.new
-    the_bookmark.user_id = session.fetch(":user_id")
+    the_bookmark.user_id = session.fetch("user_id")
     the_bookmark.movie_id = params.fetch("query_movie_id")
 
     if the_bookmark.valid?
@@ -33,6 +55,11 @@ class BookmarksController < ApplicationController
   end
 
   def update
+
+    #@current_user =User.where({ :id => session[:user_id ]}).at(0)
+
+    #self.load_current_user
+
     the_id = params.fetch("path_id")
     the_bookmark = Bookmark.where({ :id => the_id }).at(0)
 
@@ -48,6 +75,11 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+
+    #@current_user =User.where({ :id => session[:user_id ]}).at(0)
+
+    #self.load_current_user
+
     the_id = params.fetch("path_id")
     the_bookmark = Bookmark.where({ :id => the_id }).at(0)
 
